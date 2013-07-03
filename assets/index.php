a017d689e26bb5ae71c2b8849c014844299b423a
Curtis LaMalfa <curtis@kmni.com>
2013-07-03 11:30:05 -0700

    Initial Commit

---
+++ assets/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Lazy Loper</title>
+        <meta charset="utf-8" />
+        <link rel="Shortcut Icon" type="image/ico" href="assets/images/favicon.png" />
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+        <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/js/theme.js"></script>
+        <script type="text/javascript" src="assets/js/rounded.js"></script>
+        <!--[if IE 7]><link rel="stylesheet" href="assets/css/ie/ie7.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 7]><link rel="stylesheet" href="assets/css/ie/ie6.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 6]><script type="text/javascript">DD_roundies.addRule('.pngfix');</script><![endif]-->
+    </head>
+    <body>
+        <div id="main-body">
+        <div id="wrapper">
+            <div id="header">
+                <div id="nav-header">
+                    <div id="logo" class="floatleft">
+                        <a href="#" title=""><img src="assets/images/logo.png" alt="" /></a>
+                    </div>
+                    
+                    <div id="navigation" class="floatright">
+                        <ul>
+                            <li><a href="#" title="">Our Mares</a></li>
+                            <li><a href="#" title="">Foal Showcase</a></li>
+                            <li><a href="#" title="">Sale Horses</a></li>
+                            <li><a href="#" title="">About Lazy Loper</a></li>
+                            <li><a href="#" title="">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="header-content">
+                    <p>
+                        A living legend. 210 World Championships, Congress Championships and Reserve Championships. 2008 NSBA Hall-of-Fame Inductee.  Make his story a part of yours.
+                    </p>
+                </div>
+            </div>
+        </div><!--/wrapper  -->
+        </div><!--/main-body  -->
+        
+        <div id="content">
+            
+            <div id="content-one" class="floatleft">
+                <h2>Recent Winners</h2>
+                <a href="#" title=""><img src="assets/images/horse-one.jpg" alt="" /></a>
+                <a href="#" title=""><img src="assets/images/horse-two.jpg" alt="" /></a>
+                <a href="#" title=""><img src="assets/images/horse-three.jpg" alt="" /></a>
+                <a href="#" title=""><img src="assets/images/horse-four.jpg" alt="" /></a>
+                <a href="#" title=""><img src="assets/images/horse-five.jpg" alt="" /></a>
+                <a href="#" title=""><img src="assets/images/horse-six.jpg" alt="" /></a>
+            </div>
+            
+            <div id="content-two" class="floatleft">
+                <h2>Latest News</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.<br/><br/>
+
+Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.<br/><br/>
+
+    Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer leo pede, ornare a, lacinia eu, vulputate vel, nisl.
+                </p>
+            </div>
+            
+        </div><!--/content  -->
+        
+        <div id="footer">
+            <div id="footer-nav">
+                <ul>
+                    <li><a href="our_mares.php" title="">Our Mares</a></li>
+                    <li><a href="#" title="">Foal Showcase</a></li>
+                    <li><a href="#" title="">Sale Horses</a></li>
+                    <li><a href="#" title="">Our Farm</a></li>
+                    <li><a href="#" title="">Contact Us</a></li>
+                </ul>
+                <p>(C) 2010 invitation Only LLC - All Rights Reserved</p>
+            </div>
+        </div><!--/footer  -->
+    </body>
+</html>
---
+++ assets/our-mares.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Our Mares</title>
+        <meta charset="utf-8" />
+        <link rel="Shortcut Icon" type="image/ico" href="assets/images/favicon.png" />
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+        <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/js/theme.js"></script>
+        <script type="text/javascript" src="assets/js/rounded.js"></script>
+        <!--[if IE 7]><link rel="stylesheet" href="assets/css/ie/ie7.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 7]><link rel="stylesheet" href="assets/css/ie/ie6.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 6]><script type="text/javascript">DD_roundies.addRule('.pngfix');</script><![endif]-->
+    </head>
+    <body>
+        <div id="main-body">
+        <div id="wrapper">
+            <div id="header">
+                <div id="nav-header">
+                    <div id="logo" class="floatleft">
+                        <a href="#" title=""><img src="assets/images/logo.png" alt="" /></a>
+                    </div>
+                    
+                    <div id="navigation" class="floatright">
+                        <ul>
+                            <li><a href="our_mares.php" title="">Our Mares</a></li>
+                            <li><a href="#" title="">Foal Showcase</a></li>
+                            <li><a href="#" title="">Sale Horses</a></li>
+                            <li><a href="#" title="">About Lazy Loper</a></li>
+                            <li><a href="#" title="">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+        </div><!--/wrapper  -->
+        
+        </div><!--/main-body  -->
+        
+        <div id="content-mares">
+            <div id="header-content-mares" class="">
+                <h2>Our Mares</h2>
+                <div id="post-block">
+                    <h2>Gotta Love Em Lazy</h2>
+                    
+                    <div id="post-one" class="floatleft" >
+                        <img src="assets/images/mares-horse-1.jpg" alt="" />
+                    </div>
+
+                    <div id="post-two" class="floatleft">
+                        <iframe width="320" height="250" src="http://www.youtube.com/embed/nhOo1ZtrH8c" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <div id="caption" class="floatleft">
+                        <p>
+                            Half sister to ONLY BECAUSE IM GOOD Congress Top Five, Congress Top Ten, Reichert Celebration
+                        </p>
+                    </div>
+                </div><!--/post-block  -->
+
+                <div id="post-block-two">
+                    <div id="post-block-duo" class="">
+                        <h2>Only So Good</h2>
+                        <a href="#" title=""><img src="assets/images/mares-horse-3.jpg" alt="" /></a>
+                        <div id="caption2" class="floatleft">
+                        <p>
+                            Dam is full sister to Chex Are Good, producer of CHEX OUT MY LOPE Southern Belle Futurity Champion, World Show Qualifier and Reichert Top Ten.
+                        </p>
+                    </div>
+                    </div><!--/post-block-duo  -->
+
+                    <div id="post-block-duo" class="margin-left">
+                        <h2>Only Gotta Ask</h2>
+                        <a href="#" title=""><img src="assets/images/mares-horse-4.jpg" alt="" /></a>
+                        <div id="caption2" class="floatleft">
+                        <p>
+                            Half brother to Congress Top Tens, Performance ROMs, earners of over 270 points and out of a Congress Top Ten placer and AQHA High Point Top Five.
+                        </p>
+                    </div>
+                    </div><!--/post-block-duo  -->
+                </div>
+                
+                <div id="post-block" class="">
+                    <h2>Ill Make Ya Hot</h2>
+                    
+                    <div id="post-one" class="floatleft" >
+                        <img src="assets/images/mares-horse-2.jpg" alt="" />
+                    </div>
+
+                    <div id="post-two" class="floatleft">
+                        <iframe width="320" height="250" src="http://www.youtube.com/embed/nhOo1ZtrH8c" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    
+                    <div id="caption" class="floatleft">
+                        <p>
+                            Out of a World Show Qualifier and Performance ROM earner.
+                        </p>
+                    </div>
+                </div><!--/post-block  -->
+
+            </div>
+        </div>
+        
+        <div id="footer" class="mares-footer">
+            <div id="footer-nav">
+                <ul>
+                    <li><a href="our-mares.php" title="">Our Mares</a></li>
+                    <li><a href="#" title="">Foal Showcase</a></li>
+                    <li><a href="#" title="">Sale Horses</a></li>
+                    <li><a href="#" title="">Our Farm</a></li>
+                    <li><a href="#" title="">Contact Us</a></li>
+                </ul>
+                <p>(C) 2010 invitation Only LLC - All Rights Reserved</p>
+            </div>
+        </div>
+        
+    </body>
+</html>
